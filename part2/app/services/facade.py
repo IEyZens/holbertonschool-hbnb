13d6a3bbde583c8f667d61c753ec8c7c7a6b7338
IEyZens <roncint61@gmail.com>
2025-06-16 05:27:38 +0200

    :hammer: feat: add amenity management methods to HBnBFacade class

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -18,3 +18,19 @@ class HBnBFacade:
 
     def update_user(self, user_id, data):
         return self.user_repo.update(user_id, data)
+
+    def create_amenity(self, amenity_data):
+        # Espace réservé pour la logique de création d’une commodité
+        pass
+
+    def get_amenity(self, amenity_id):
+        # Espace réservé pour la logique de récupération d’une commodité par ID
+        pass
+
+    def get_all_amenities(self):
+        # Espace réservé pour la logique de récupération de toutes les commodités
+        pass
+
+    def update_amenity(self, amenity_id, amenity_data):
+        # Espace réservé pour la logique de mise à jour d’une commodité
+        pass
