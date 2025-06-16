12c93fcff17c0f5ec47d16da4c8e4ed11654980c
IEyZens <roncint61@gmail.com>
2025-06-16 03:49:11 +0200

    :hammer: refactor: improve validation error messages in Place class constructor

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -1,27 +1,29 @@
 from .base_model import BaseModel
+from .review import Review
+from .amenity import Amenity
+from .user import User
 
 class Place(BaseModel):
     def __init__(self, title, description, price, latitude, longitude, owner, max_person):
         super().__init__()
 
         if not title or len(title) > 100:
-            raise ValueError("Invalid title: The title must be less than or equal to 100 characters.")
+            raise ValueError("Invalid title: must be a non-empty string up to 100 characters.")
 
         if price < 0:
-            raise ValueError("Invalid price: The price must be positive.")
+            raise ValueError("Invalid price: must be a number greater than or equal to 0.")
 
-        if latitude < 90 or latitude > -90:
-            raise ValueError("Invalid latitude: Latitude must be between -90 and 90.")
+        if latitude < -90 or latitude > 90:
+            raise ValueError("Invalid latitude: must be a float between -90 and 90 degrees.")
 
-        if longitude < 180 or longitude > -180:
-            raise ValueError("Invalid longitude: Longitude must be between -180 and 180.")
+        if longitude < -180 or longitude > 180:
+            raise ValueError("Invalid longitude: must be a float between -180 and 180 degrees.")
 
-        if owner is False:
-            raise TypeError("Invalid owner: You must be the owner of the site.")
+        if not isinstance(owner, User):
+            raise TypeError("Invalid owner: must be an instance of the User class.")
 
         if max_person < 1:
-            raise ValueError("Invalid capacity: The maximum number of people must be at least 1.")
-
+            raise ValueError("Invalid capacity: must be at least 1 person.")
 
         self.title = title
         self.description = description
@@ -34,7 +36,11 @@ class Place(BaseModel):
         self.amenities = []
 
     def add_review(self, review):
+        if not isinstance(review, Review):
+            raise TypeError("Invalid review: must be an instance of the Review class.")
         self.reviews.append(review)
 
     def add_amenity(self, amenity):
+        if not isinstance(amenity, Amenity):
+            raise TypeError("Invalid amenity: must be an instance of the Amenity class.")
         self.amenities.append(amenity)
