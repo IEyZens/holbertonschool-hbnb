3376711d4f79a43b4d2045be3cce9590224d8cb2
IEyZens <roncint61@gmail.com>
2025-06-16 03:11:44 +0200

    :bulb: refactor: update placeholder comment for API namespaces in __init__.py

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -5,7 +5,6 @@ def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
-    # Placeholder for API namespaces (endpoints will be added later)
-    # Additional namespaces for places, reviews, and amenities will be added later
+    # Espace réservé pour les namespaces de l’API
 
     return app
