ab635ee7b92221f17a24a64f7b97c863d9880da5
IEyZens <roncint61@gmail.com>
2025-06-16 03:45:42 +0200

    :hammer: refactor: move review text validation to constructor in Review class

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -1,13 +1,22 @@
 from .base_model import BaseModel
+from .user import User
+from .place import Place
 
 class Review(BaseModel):
     def __init__(self, text, rating, place, user):
         super().__init__()
 
+        if not text or len(text) < 1:
+            raise ValueError("Invalid review text: 'text' must be a non-empty string.")
+
         if rating < 1 or rating > 5:
             raise ValueError("Invalid rating: The rating must be between 1 and 5.")
 
-        
+        if not isinstance(user, User):
+            raise TypeError("Invalid user: must be an instance of the User class.")
+
+        if not isinstance(place, Place):
+            raise TypeError("Invalid place: must be an instance of the Place class.")
 
         self.text = text
         self.rating = rating
