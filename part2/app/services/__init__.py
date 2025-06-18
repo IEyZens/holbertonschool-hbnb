d81e94f05c035a321be7ddede64bcad05dcf6e28
IEyZens <roncint61@gmail.com>
2025-06-18 02:53:42 +0200

    :bulb: refactor: enhance comments for clarity in service initialization

--- part2/app/services/__init__.py
+++ part2/app/services/__init__.py
@@ -1,3 +1,5 @@
+# Importation de la façade principale regroupant la logique métier
 from app.services.facade import HBnBFacade
 
+# Instanciation d’un objet unique de la façade pour centraliser les opérations métier
 facade = HBnBFacade()
