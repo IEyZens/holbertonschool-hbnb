1851c3724d07fc4de2215c01c6056f6b1e455ac0
IEyZens <roncint61@gmail.com>
2025-07-05 00:51:03 +0200

    feat: enhance docstring for get_user_by_email method in UserRepository

--- part3/app/persistence/user_repository.py
+++ part3/app/persistence/user_repository.py
@@ -1,10 +1,19 @@
 from app.models.user import User
-from app import db
 from app.persistence.repository import SQLAlchemyRepository
+
 
 class UserRepository(SQLAlchemyRepository):
     def __init__(self):
         super().__init__(User)
 
     def get_user_by_email(self, email):
+        """
+        Retrieve a user by email address.
+
+        Args:
+            email (str): The user's email.
+
+        Returns:
+            User or None: The user instance if found, else None.
+        """
         return self.model.query.filter_by(email=email).first()
