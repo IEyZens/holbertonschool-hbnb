6518210189f9371a61bee3f817fbafdd9adbd455
IEyZens <roncint61@gmail.com>
2025-07-04 14:51:32 +0200

    feat: initialize SQLAlchemy instance and integrate with Flask app

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -9,6 +9,7 @@ from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 from app.api.v1.users import api as users_ns
 from app.api.v1.admin_users import api as admin_users_ns
 from app.api.v1.places import api as places_ns
@@ -21,6 +22,7 @@ from app.api.v1.auth import api as auth_ns
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
+db = SQLAlchemy()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -57,6 +59,7 @@ def create_app(config_class="config.DevelopmentConfig"):
 
     bcrypt.init_app(app)
     jwt.init_app(app)
+    db.init_app(app)
 
     # Enregistrement du namespace utilisateurs (v1)
     api.add_namespace(users_ns, path='/api/v1/users')
