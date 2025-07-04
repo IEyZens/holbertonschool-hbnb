8f5d5b0387ae9b2e7a5afa376697828a5e0a9536
IEyZens <roncint61@gmail.com>
2025-07-04 15:11:48 +0200

    refactor: remove direct instantiation of bcrypt, jwt, and db in __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -7,9 +7,7 @@ registering versioned namespaces for user, place, amenity, and review resources.
 
 from flask import Flask
 from flask_restx import Api
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
-from flask_sqlalchemy import SQLAlchemy
+from app.extensions import db, bcrypt, jwt
 from app.api.v1.users import api as users_ns
 from app.api.v1.admin_users import api as admin_users_ns
 from app.api.v1.places import api as places_ns
@@ -19,10 +17,6 @@ from app.api.v1.admin_amenities import api as admin_amenities_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.admin_reviews import api as admin_reviews_ns
 from app.api.v1.auth import api as auth_ns
-
-bcrypt = Bcrypt()
-jwt = JWTManager()
-db = SQLAlchemy()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
