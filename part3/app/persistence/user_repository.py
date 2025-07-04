65deb066730fc1b623fa9ca917f2f5323381c072
IEyZens <roncint61@gmail.com>
2025-07-04 18:39:17 +0200

    refactor: remove model parameter from UserRepository constructor

--- part3/app/persistence/user_repository.py
+++ part3/app/persistence/user_repository.py
@@ -3,8 +3,7 @@ from app import db
 from app.persistence.repository import SQLAlchemyRepository
 
 class UserRepository(SQLAlchemyRepository):
-    def __init__(self, model):
-        from app.models.user import User
+    def __init__(self):
         super().__init__(User)
 
     def get_user_by_email(self, email):
