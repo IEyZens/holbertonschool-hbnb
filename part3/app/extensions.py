39508bcf879a6d2ac4c72cfa5a0086e8f32c9b81
IEyZens <roncint61@gmail.com>
2025-07-13 21:05:32 +0200

    refactor: update docstring for centralized extension initialization in extensions.py

--- part3/app/extensions.py
+++ part3/app/extensions.py
@@ -1,3 +1,10 @@
+"""
+Centralized extension initialization for the HBnB Flask application.
+
+This module defines instances of Flask extensions (SQLAlchemy, Bcrypt, JWTManager)
+to be initialized within the application factory.
+"""
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
